e120d7db744880358e973a84b2be362312b5466a
Darlei Kroth <maildarlei@icloud.com>
2020-05-12 18:01:32 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import jsFormatter from './lib/formatter';
+import jsGuid from './lib/guid';
+
+export const formatter = jsFormatter;
+export const generateGUID = jsGuid;
+
+export default {
+  formatter,
+  generateGUID,
+};
